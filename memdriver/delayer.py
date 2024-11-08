0af5d2c0540d6b2ea70b55eb39dec5223d5c0114
Max Baginskiy <kachan1208@gmail.com>
2024-11-08 23:39:54 +0200

    working on a cron test and delayer

--- memdriver/delayer.py
+++ memdriver/delayer.py
@@ -1,20 +1,58 @@
 import asyncio
+from asyncio.queues import Queue
+
+from cmd_commit import commit_state_ctx, commit
 from engine import Engine
 from doer import Doer, ErrCommandNotSupported
 from command import Command
-from cmd_delay import DelayCommand
+from cmd_delay import DelayCommand, DelayCommitAnnotation
+from errors import ErrCommitConflict
 
 
 class Delayer(Doer):
     def __init__(self):
         self.done_event: asyncio.Event = asyncio.Event()
-        self.e: Engine
+        self.e: Engine | None = None
 
-    def do(self, cmd: Command):
+    async def do(self, cmd: Command):
         if not isinstance(cmd, DelayCommand):
             raise ErrCommandNotSupported
 
         try:
             cmd.prepare()
+            ticks, ticks_stop = ticker(cmd.duration)
+            while await ticks.get():
+                if (
+                    cmd.delayed_state_ctx.current.transition.annotations[
+                        DelayCommitAnnotation
+                    ]
+                    == "True"
+                ):
+                    self.e.do(commit(commit_state_ctx(cmd.delay_state_ctx)))
+                    self.e.execute(cmd.delay_state_ctx)
+
+            ticks_stop.set()
         except Exception as e:
-            raise e
+            raise Exception(f"ERROR: memdriver: delayer: engine: %{e}")
+
+    def init(self, e: Engine):
+        self.e = e
+
+    def shutdown(self):
+        self.done_event.set()
+
+
+def ticker(delay: float):
+    q = Queue(maxsize=1)
+    c = asyncio.Event()
+
+    async def tick():
+        t: int = 0
+        while not c.is_set():
+            t += delay
+            await asyncio.sleep(delay)
+            if not q.full():
+                await q.put(t)
+
+    asyncio.create_task(tick())
+    return q, c
